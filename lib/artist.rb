a25c27449a12a20abd33e1b24d4678b5d040f8c3
Amal Abdu <abduamal@ymail.com>
2020-05-04 03:34:01 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Song
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(title)
+    created_song = Song.new(title)
+    created_song.save
+    created_song
+  end
+
+end
