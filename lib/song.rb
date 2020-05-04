dd9d5db9a65d920c1fdcd6abafa4563c9b90ebf4
Amal Abdu <abduamal@ymail.com>
2020-05-04 05:58:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
 
   def genre=(genre)
     @genre = genre
-    genre.songs.push self unless genre.songs.include? self
+    genre.songs << self unless genre.songs.include? (self)
   end
 
   def self.find_by_name(name)
