264c012bec278dbef09e623d29b4acec628c91b4
Amal Abdu <abduamal@ymail.com>
2020-05-04 05:27:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,7 @@ class Song
     genre.songs.push self unless genre.songs.include? self
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
     @@all.detect do |song|
       song.name == name
     end
