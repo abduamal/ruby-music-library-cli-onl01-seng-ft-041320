b228da3ffa4206aee35f0fd27247ba7eda7d4449
Amal Abdu <abduamal@ymail.com>
2020-05-04 05:58:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,13 +52,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    @@all.detect do |song|
-      if song.name == name
-        song
-      else
-        self.create(name)
-      end
-    end
+    
   end
 
 end
