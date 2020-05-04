06907f19067a6ad9e3210df75715395cfd235655
Amal Abdu <abduamal@ymail.com>
2020-05-04 05:50:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,6 +20,12 @@ class Song
     @@all << self
   end
 
+  def self.create(name)
+    song = new(name)
+    song.save
+    song
+  end
+
   def self.destroy_all
     @@all.clear
   end
@@ -40,12 +46,6 @@ class Song
   def genre=(genre)
     @genre = genre
     genre.songs.push self unless genre.songs.include? self
-  end
-
-  def self.create(name)
-    song = new(name)
-    song.save
-    song
   end
 
   def self.find_by_name(name)
