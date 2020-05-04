9ecaa38f2a82b07ccc8378c73bae61a54c8ab497
Amal Abdu <abduamal@ymail.com>
2020-05-04 03:36:58 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,30 @@
+class Genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  # def self.create(name)
+  #   discovery = Genre.new(name)
+  #   discovery.save
+  #   discovery
+  # end
+
+end
